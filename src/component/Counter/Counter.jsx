09da670967785c3c6e7482ea92bf2f1238116ff9
Abhijith N B <www.abhijithbose@gmail.com>
2023-06-23 13:07:56 +0530

    useReducer created

--- src/component/Counter/Counter.jsx
+++ src/component/Counter/Counter.jsx
@@ -1,65 +1,106 @@
-import React from "react";
-import { useState } from "react";
+import React, { useReducer } from "react";
+// import { useState } from "react";
 import "./Counter.css";
 
 export const Counter = () => {
-    const [count, setCount] = useState(0);
+    const counterReducer = (state, action) => {
+        switch (action.type) {
+            case "decrement":
+                if (state.count > 0) {
+                    const newcount = state.count - 1;
+                    return {
+                        count: newcount,
+                    };
+                } else {
+                    return {
+                        count: state.count,
+                    };
+                }
 
-    const onDecrement = () => {
-        if (count > 0) {
-            setCount((prev) => prev - 1);
+            case "increment":
+                if (state.count <= 99) {
+                    const newCount = state.count + 1;
+                    return {
+                        count: newCount,
+                    };
+                } else {
+                    return {
+                        count: state.count,
+                    };
+                }
+
+            case "incrementBy":
+                if (state.count <= 90) {
+                    const newCount = state.count + action.value;
+                    return {
+                        count: newCount,
+                    };
+                } else {
+                    return {
+                        count: state.count,
+                    };
+                }
+
+            case "reset":
+                return {
+                    count: 0,
+                };
+
+            case "decrementBy":
+                if (state.count >= 5) {
+                    const newCount = state.count - action.value;
+                    return {
+                        count: newCount,
+                    };
+                } else {
+                    return {
+                        count: state.count,
+                    };
+                }
+
+            default:
+                return state;
         }
     };
-    const onIncrement = () => {
-        if (count <= 99) {
-            setCount((prev) => prev + 1);
-        }
-    };
-    const onReset = () => {
-        setCount(0);
-    };
-    const onIncrementBy = (value) => {
-        if (count <= 90) {
-            setCount((prev) => prev + value);
-        }
-    };
-    const onDecrementBy = (value) => {
-        if (count >= 5) {
-            setCount((prev) => prev - value);
-        }
-    };
+
+    const [state, dispatch] = useReducer(counterReducer, {
+        count: 0,
+    });
 
     return (
         <div className="counter-container">
             <div className="loading-container">
-                <p> {count}</p>
+                <p> {state.count}</p>
                 <div
                     className="loading"
                     style={{
-                        height: 3 * count,
+                        height: 3 * state.count,
                         background:
-                            count < 60
+                            state.count < 60
                                 ? "green"
-                                : count >= 60 && count <= 75
+                                : state.count >= 60 && state.count <= 75
                                 ? "rgb(255, 162, 0)"
-                                : count > 75 && count < 90
+                                : state.count > 75 && state.count < 90
                                 ? "orangered"
                                 : "red",
                     }}
                 ></div>
             </div>
             <div className="buttons">
-                <button onClick={() => onDecrementBy(5)}>Decrement By 5</button>
-                <button onClick={onDecrement} style={{ fontSize: "25px" }}>
+                <button onClick={() => dispatch({ type: "decrementBy", value: 5 })}>Decrement By 5</button>
+                <button onClick={() => dispatch({ type: "decrement" })} style={{ fontSize: "25px" }}>
                     -
                 </button>
-                <button onClick={onReset} style={{ background: "rgb(226, 36, 36)" , fontSize:"14px"}}>
+                <button
+                    onClick={() => dispatch({ type: "reset" })}
+                    style={{ background: "rgb(226, 36, 36)", fontSize: "14px" }}
+                >
                     Reset
                 </button>
-                <button onClick={onIncrement} style={{ fontSize: "25px" }}>
+                <button onClick={() => dispatch({ type: "increment" })} style={{ fontSize: "25px" }}>
                     +
                 </button>
-                <button onClick={() => onIncrementBy(10)}>Increment By 10</button>
+                <button onClick={() => dispatch({ type: "incrementBy", value: 10 })}>Increment By 10</button>
             </div>
         </div>
     );
